8cf839ac9c57f7d4148150aa87ab0c1c7efeca44
Oskru <oskrupaa@gmail.com>
2022-06-01 19:57:55 +0200

    TODO: Next path

--- carts/urls.py
+++ carts/urls.py
@@ -7,3 +7,5 @@ urlpatterns = [
     path('remove_cart/<int:product_id>/', views.remove_cart, name='remove_cart'),
     path('remove_cart_item/<int:product_id>/', views.remove_cart_item, name='remove_cart_item'),
 ]
+
+# TODO: Next path (ik)
